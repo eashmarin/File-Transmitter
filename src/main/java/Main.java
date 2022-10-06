6f8792be69d7ee4ebd6dfa041b5570ebeac89fd1
Evgeny Ashmarin <evgeniiashmarin@gmail.com>
2022-10-06 22:27:21 +0700

    log4j2 fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,6 @@ import org.apache.logging.log4j.LogManager;
 
 public class Main {
     public static void main(String[] args) {
-        LogManager.getRootLogger().debug("t5555");
+        LogManager.getLogger().debug("t5555");
     }
 }
